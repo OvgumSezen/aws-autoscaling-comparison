a8fd0715a1f8aeba54d950eb3c907053dc56a1f3
OvgumSezen <ovgumcansezen@gmail.com>
2025-05-29 12:49:04 +0200

    - Added randomized CPU/memory load to REST endpoint of the three services.

--- ms-2/src/main/java/com/autoscaling_comparison/ms_2/controller/MS2Controller.java
+++ ms-2/src/main/java/com/autoscaling_comparison/ms_2/controller/MS2Controller.java
@@ -9,8 +9,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/ms2")
 public class MS2Controller {
+    long min = 1000000;
+    long max = 10000000;
+    long range = max - min + 1;
+
     @GetMapping
     public List<String> getMS2() {
+        simulateLoad();
         return List.of("MS2_response1", "MS2_response2", "MS2_response3");
+    }
+
+    private void simulateLoad() {
+        long sum = 0;
+        long randomLoad = (long) (Math.random() * range) + min;
+        for (int i = 0; i < randomLoad; i++) {
+            sum += i;
+        }
     }
 }
--- ms-3/src/main/java/com/autoscaling_comparison/ms_3/controller/MS3Controller.java
+++ ms-3/src/main/java/com/autoscaling_comparison/ms_3/controller/MS3Controller.java
@@ -9,8 +9,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/ms3")
 public class MS3Controller {
+    long min = 1000000;
+    long max = 10000000;
+    long range = max - min + 1;
+
     @GetMapping
     public List<String> getMS3() {
+        simulateLoad();
         return List.of("MS3_response1", "MS3_response2", "MS3_response3");
+    }
+
+    private void simulateLoad() {
+        long sum = 0;
+        long randomLoad = (long) (Math.random() * range) + min;
+        for (int i = 0; i < randomLoad; i++) {
+            sum += i;
+        }
     }
 }
